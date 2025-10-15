fd310e5a3f6582de9f8115b5ed83b4bdd4c99405
viet2005-68 <23020170@vnu.edu.vn>
2025-10-15 08:17:30 +0700

    initial frontend

---
+++ Frontend/src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <>
+      <div className="bg-sky-300 min-h-screen flex flex-col items-center justify-center">
+        DCM QUA MET
+      </div>
+    </>
+  );
+}
+
+export default App;
