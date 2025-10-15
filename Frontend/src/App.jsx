daf3579f8c78fbb9951ae3d0f72adb783fab4db9
viet2005-68 <23020170@vnu.edu.vn>
2025-10-15 09:11:22 +0700

    change v tailwind to 4

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,7 +1,7 @@
 function App() {
   return (
     <>
-      <div className="bg-sky-300 min-h-screen flex flex-col items-center justify-center">
+      <div className="bg-linear-to-r from-orange-200 to-green-300 min-h-screen flex flex-col items-center justify-center text-4xl text-green-900 h-15">
         DCM QUA MET
       </div>
     </>
