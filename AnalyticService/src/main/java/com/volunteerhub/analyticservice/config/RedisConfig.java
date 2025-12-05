3a35183a2fb5b3498e473ddc1a773b4ee3552b5e
tungluuquang <tungq2005@gmail.com>
2025-12-05 21:21:18 +0700

    [BE] fix env bugs

--- AnalyticService/src/main/java/com/volunteerhub/analyticservice/config/RedisConfig.java
+++ AnalyticService/src/main/java/com/volunteerhub/analyticservice/config/RedisConfig.java
@@ -1,23 +1,18 @@
 package com.volunteerhub.analyticservice.config;
 
-import feign.RequestInterceptor;
-import feign.RequestTemplate;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.GenericToStringSerializer;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationToken;
 
 @Configuration
 public class RedisConfig {
 
     @Bean
-    public RedisTemplate<String, Integer> stringIntegerRedisTemplate(RedisConnectionFactory connectionFactory) {
-        RedisTemplate<String, Integer> template = new RedisTemplate<>();
+    public RedisTemplate<String, Long> stringIntegerRedisTemplate(RedisConnectionFactory connectionFactory) {
+        RedisTemplate<String, Long> template = new RedisTemplate<>();
         template.setConnectionFactory(connectionFactory);
         // Use String serializer for keys
         template.setKeySerializer(new StringRedisSerializer());
