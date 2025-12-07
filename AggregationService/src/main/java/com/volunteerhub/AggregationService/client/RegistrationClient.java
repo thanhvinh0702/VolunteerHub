8e062dea28707030e448863fa3686adeaad68dac
thanhvinh0702 <thanhvinhhero0702@gmail.com>
2025-12-08 00:22:29 +0700

    refactor(aggregation-service): Refactor Feign client

--- AggregationService/src/main/java/com/volunteerhub/AggregationService/client/RegistrationClient.java
+++ AggregationService/src/main/java/com/volunteerhub/AggregationService/client/RegistrationClient.java
@@ -6,7 +6,6 @@ import com.volunteerhub.common.dto.PageResponse;
 import com.volunteerhub.common.dto.UserEventResponse;
 import com.volunteerhub.common.enums.UserEventStatus;
 import com.volunteerhub.common.dto.RegistrationResponse;
-import com.volunteerhub.common.enums.UserEventStatus;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -14,32 +13,32 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
 
-@FeignClient(name = "REGISTRATIONSERVICE", configuration = FeignConfig.class)
+@FeignClient(name = "REGISTRATIONSERVICE", path = "/api/v1/registrations", configuration = FeignConfig.class)
 public interface RegistrationClient {
 
-    @GetMapping("/api/v1/registrations")
+    @GetMapping
     PageResponse<UserEventResponse> findAllByUserId(@RequestParam(required = false) UserEventStatus status,
                                                     @RequestParam(required = false) Integer pageNum,
                                                     @RequestParam(required = false) Integer pageSize);
 
-    @GetMapping("/api/v1/registrations/events/registration-count")
+    @GetMapping("/events/registration-count")
     List<EventRegistrationCount> getEventsParticipantCounts(@RequestParam(required = false) List<Long> eventIds,
                                                             @RequestParam(required = false) Integer pageNum,
                                                             @RequestParam(required = false) Integer pageSize,
                                                             @RequestParam(required = false) Integer days);
 
-    @GetMapping("/api/v1/registrations/events/{eventId}/user-ids")
+    @GetMapping("/events/{eventId}/user-ids")
     List<String> findUserIdsByEventId(@PathVariable Long eventId,
                                       @RequestParam(required = false) Integer pageNum,
                                       @RequestParam(required = false) Integer pageSize);
 
-    @GetMapping("/api/v1/registrations/events/{eventId}")
+    @GetMapping("/events/{eventId}")
     PageResponse<UserEventResponse> findAllByEventId(@PathVariable Long eventId,
                                                      @RequestParam(required = false) UserEventStatus status,
                                                      @RequestParam(required = false) Integer pageNum,
                                                      @RequestParam(required = false) Integer pageSize);
 
-    @GetMapping("/api/v1/registrations/internal/manager")
+    @GetMapping("/internal/manager")
     List<RegistrationResponse> getRegistrationsByOwnerId(@RequestParam(value = "eventId", required = false) Long eventId,
                                                          @RequestParam(value = "status", required = false) UserEventStatus status,
                                                          @RequestParam("pageNum") Integer pageNum,
